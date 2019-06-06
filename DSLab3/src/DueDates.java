d71d0ccaf2491400ac909802fe40bc0af8cbabe4
ifearthemeow <47339223+ifearthemeow@users.noreply.github.com>
2019-06-06 15:05:41 -0400

    Add files via upload

---
+++ DSLab3/src/DueDates.java
@@ -0,0 +1,96 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+/************************************************************************************************************
+Purpose:  This class will model a due dates for assessments in a course
+Author:  Linda Crane and Ababiya Abajobir
+Course: CST8130
+Lab Section: 300
+Data members:  
+
+Methods: 
+DueDates() - default constructor. Initializes object array to 10 by default.
+
+DueDates(int max) - parameterized constructor. Initializes object array to given value.
+
+inputDueDates(Scanner in) - boolean method. Stores assessment dates in the object array.       
+
+addOne () - void method. Increments the assessment dates in the object array.
+
+toString() - String method. Displays the content of the object array.
+ *************************************************************************************************************/
+
+public class DueDates {
+	private MyDate[] dueDates ;
+	private int numDates;
+
+	public DueDates() {
+		//*****  write the code for this method here
+		dueDates = new MyDate[10];
+		numDates = 0;
+	}
+
+	public DueDates(int max) {
+		//*****  write the code for this method here
+		this.dueDates = new MyDate[max];
+		numDates = 0;
+	}
+
+	public boolean inputDueDates(Scanner in, boolean check) {
+		MyDate date = new MyDate();
+		date.inputDate(in, check);
+		dueDates[numDates] = date;
+		numDates++;
+		return true;
+		//*****  write the code for this method here
+	}
+
+	public void addOne () {
+		//*****  write the code for this method here
+		for(int i = 0; i < dueDates.length;i++)
+			dueDates[i].addOne();
+	}
+
+	public String toString() {
+		String dates = "";
+		for(int i = 0; i < dueDates.length;i++)
+			System.out.println((i+1)+": "+ dueDates[i].toString());
+
+		return dates;
+		//*****  write the code for this method here
+	}
+	
+	public void sortDate () {
+		//*****  write the code for this method here
+		for(int i = 1; i < dueDates.length;i++){
+			for(int j = i; j>0; j--)
+			if(dueDates[j-1].isGreaterThan(dueDates[j])){
+				MyDate temp = dueDates[j-1];
+				dueDates[j-1] = dueDates[j];
+				dueDates[j] = temp;
+			}
+			
+		}
+	}
+
+	public void writeFile(FileWriter o){
+
+		try {
+			o.append(dueDates.length + "\n");
+			for(int i = 0; i < dueDates.length; i++) {
+				o.append(dueDates[i].toString().replaceAll("/", "\n"));
+				if(((dueDates.length - i) > 1))
+					o.append("\n");
+			}
+			o.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
---
+++ DSLab3/src/MyDate.java
@@ -0,0 +1,144 @@
+
+import java.util.Scanner;
+/************************************************************************************************************
+Purpose:  This class will model a simple date by keeping day, month and year information.   Leap years are NOT
+               accommodated in this class.
+Author:  Linda Crane and Ababiya Abajobir
+Course: CST8130
+Lab Section: 300
+Data members:  day : int - value between 1 and 31 inclusive (depending on value in month)
+               month: int - value between 1 and 12 inclusive
+               year: int - positive value
+Methods: default constructor - sets date to Jan 1, 2018
+         toString (): String - prints date in year/moht/day format
+         inputDate(Scanner): boolean - reads a valid date from the Scanner parameter and returns through
+                                       boolean success or not
+         addOne(): void - adds one to the day field and then adjusts month and year as needed.                                              
+
+
+ *************************************************************************************************************/
+public class MyDate {
+	private int day = 1;
+	private int month = 1;
+	private int year = 2018;
+
+	public MyDate() {
+	}
+
+	public String toString() {   return new String ("" + year + "/" + month + "/" + day);
+	}
+
+	public boolean inputDate(Scanner in, boolean check) {
+		month = 0;
+		day = 0; 
+		year = 0;
+		do {
+			if(check==false)
+				System.out.print ("Enter year: ");
+			if (in.hasNextInt() && !check)
+				this.year = in.nextInt();
+			else if(in.hasNextLine()){
+				this.year = Integer.parseInt(in.nextLine());
+				
+			}
+			else {
+				System.out.println ("Invalid year input");
+				in.next();
+			}
+		} while (this.year <= 0);
+
+		do {
+
+			if(check==false)
+				System.out.print ("Enter month - between 1 and 12: ");
+			if (in.hasNextInt() && !check)
+				this.month = in.nextInt();
+			else if(in.hasNextLine())
+				this.month = Integer.parseInt(in.nextLine());
+			else {
+				System.out.println ("Invalid month input");
+				in.next();
+			}
+		} while (this.month <= 0 || this.month > 12);
+
+		do {
+
+			if(month == 2){
+				if (((year%4==0) && (year%100!=0)) || (year%400==0)){//it's a leap year
+					if(!check)
+						System.out.print ("Enter day - between 1 and 29: "); 
+				}
+				else
+					if(!check)
+						System.out.print ("Enter day - between 1 and 28: ");
+			}
+			else if ((month == 4) || (month == 6) || (month == 9) || (month == 11)){
+				if(!check)
+					System.out.print ("Enter day - between 1 and 30: ");
+			}
+			else if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12)){
+				if(!check)
+					System.out.print ("Enter day - between 1 and 31: ");
+			}
+			if (in.hasNextInt() && !check)
+				this.day = in.nextInt();
+			else if(in.hasNextLine())
+				this.day = Integer.parseInt(in.nextLine());
+			else {
+				System.out.println ("Invalid day input");
+				in.next();
+			}
+		} while (this.day <= 0 || this.day > 31 || (this.month == 2 && (((year%4==0) && (year%100!=0)) || (year%400==0))  && this.day > 29) || (this.month == 2 &! (((year%4==0) && (year%100!=0)) || (year%400==0))  && this.day > 28) || (this.day > 30 && (this.month == 9 ||this.month == 4 ||this.month == 6 ||this.month == 11) ) );
+
+		return true;		
+	}
+
+	public void addOne (){
+		// *********    write the code for this method here  **************
+
+		//incrementing day in order to make changes to month and year, as required
+		day++; 
+		if (day > 28){
+			if(month == 2){
+				if ((((year%4==0) && (year%100!=0)) || (year%400==0)) && day == 29){
+					//no action, it's a leap year
+				}
+				else
+				{
+					month++;
+					day = 1;
+				}
+			}
+			//there are 4 months in a year that go up to 30. We reset day to 1 and increment month.
+			else if (((month == 4) || (month == 6) || (month == 9) || (month == 11)) && day > 30){ 
+				month++;
+				day = 1;
+			}
+			else if (((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12)) && day > 31){
+				if(month == 12){ // new year - we now have to increment year as well
+					month = 1;
+					day = 1;
+					year++;
+				}
+				else{
+					month++;
+					day = 1;
+				}
+			}		
+		}			
+	}
+	
+	public boolean isGreaterThan(MyDate date) {
+		if(this.year == date.year){
+			if(this.month >= date.month)
+				if(this.day > date.day)
+					return true;
+		} else if(this.year > date.year){
+			return true;
+		}
+		
+		return false;
+	}
+}
+
+
